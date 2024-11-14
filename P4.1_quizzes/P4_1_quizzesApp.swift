725402192858cefbb919d4cd0b93383d2771d820
Alvaro Estevez Rodriguez <aestevezro@gmail.com>
2024-11-14 16:43:15 +0100

    Initial Commit

---
+++ P4.1_quizzes/P4_1_quizzesApp.swift
@@ -0,0 +1,18 @@
+//
+//  P4_1_quizzesApp.swift
+//  P4.1_quizzes
+//
+//  Created by d074 DIT UPM on 14/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct P4_1_quizzesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            QuizzesView()
+        }
+    }
+}
